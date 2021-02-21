969358786a096397c9cc9316b2e3d6e1dd9e8c66
Tom Schultz <tom@soapstonetech.com>
2021-02-21 03:30:31 -0800

    backup-vhserver.sh start after backup.

--- backup-vhserver.sh
+++ backup-vhserver.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 /home/vhserver/gsm/vhserver stop
+
 timestamp=$(date +"%Y-%m-%d-%H%M%S")
 worlds_tarball="worlds-${timestamp}.tar.gz"
 s3_prefix="s3://valheim-gsm-backups"
@@ -12,3 +13,5 @@ mkdir -p /home/vhserver/worlds-backup
 find /home/vhserver/worlds-backup -mtime 7 -type f -delete
 tar czfP /home/vhserver/worlds-backup/${worlds_tarball} /home/vhserver/.config/unity3d/IronGate/Valheim/worlds
 aws s3 cp /home/vhserver/worlds-backup/${worlds_tarball} ${s3_prefix}/worlds/$worlds_tarball
+
+/home/vhserver/gsm/vhserver start
